1cd9223ca2de9af5fb66a5d6a0cd69e39842079f
nmalzieu <noemalzieu@noe.local>
2014-02-20 16:14:40 +0100

    Changes in app + removed red and added state files folder

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import subprocess
 
 from flask import Flask
 from flask import render_template
-from PIL import Image
 
 app = Flask(__name__)
 app.debug = True
@@ -22,11 +21,14 @@ state = 0
 def is_there_red():
     return os.path.isfile('red')
 
+
 def is_there_green():
     return os.path.isfile('green')
 
+
 def is_there_green_and_red():
     return os.path.isfile('green') and os.path.isfile('red')
+
 
 def clean_red_green():
     if is_there_red():
@@ -63,7 +65,6 @@ def ready():
         os.remove('state_files/picture_taking')
         picture_taking = 0
         last_picture = pending_pictures[-1]
-        resize_and_crop(last_picture, last_picture, (600, 800))
 
     if picture_taking:
         state = 1
@@ -140,60 +141,3 @@ def ready():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
-
-
-def resize_and_crop(img_path, modified_path, size, crop_type='middle'):
-    """
-    Resize and crop an image to fit the specified size.
-
-    args:
-        img_path: path for the image to resize.
-        modified_path: path to store the modified image.
-        size: `(width, height)` tuple.
-        crop_type: can be 'top', 'middle' or 'bottom', depending on this
-            value, the image will cropped getting the 'top/left', 'middle' or
-            'bottom/right' of the image to fit the size.
-    raises:
-        Exception: if can not open the file in img_path of there is problems
-            to save the image.
-        ValueError: if an invalid `crop_type` is provided.
-    """
-    # If height is higher we resize vertically, if not we resize horizontally
-    img = Image.open(img_path)
-    # Get current and desired ratio for the images
-    img_ratio = img.size[0] / float(img.size[1])
-    ratio = size[0] / float(size[1])
-    #The image is scaled/cropped vertically or horizontally depending on the ratio
-    if ratio > img_ratio:
-        img = img.resize((size[0], round(size[0] * img.size[1] / img.size[0])),
-                Image.ANTIALIAS)
-        # Crop in the top, middle or bottom
-        if crop_type == 'top':
-            box = (0, 0, img.size[0], size[1])
-        elif crop_type == 'middle':
-            box = (0, round((img.size[1] - size[1]) / 2), img.size[0],
-                   round((img.size[1] + size[1]) / 2))
-        elif crop_type == 'bottom':
-            box = (0, img.size[1] - size[1], img.size[0], img.size[1])
-        else :
-            raise ValueError('ERROR: invalid value for crop_type')
-        img = img.crop(box)
-    elif ratio < img_ratio:
-        img = img.resize((round(size[1] * img.size[0] / img.size[1]), size[1]),
-                Image.ANTIALIAS)
-        # Crop in the top, middle or bottom
-        if crop_type == 'top':
-            box = (0, 0, size[0], img.size[1])
-        elif crop_type == 'middle':
-            box = (round((img.size[0] - size[0]) / 2), 0,
-                   round((img.size[0] + size[0]) / 2), img.size[1])
-        elif crop_type == 'bottom':
-            box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
-        else :
-            raise ValueError('ERROR: invalid value for crop_type')
-        img = img.crop(box)
-    else :
-        img = img.resize((size[0], size[1]),
-                Image.ANTIALIAS)
-        # If the scale is the same, we do not need to crop
-    img.save(modified_path)
