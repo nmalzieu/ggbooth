c80ded48bf3470af3070f401bc68bd7d547f2095
nmalzieu <noe.malzieu@gmail.com>
2014-02-21 13:51:56 +0100

    Added error and timer handling

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import json
 import time
 import subprocess
+import threading
 from PIL import Image
 
 from flask import Flask
@@ -16,9 +17,16 @@ printing_time = 65  # From reviews, printing takes 56 seconds
 global last_printing_beginning
 global state
 global wat
+global photo_timer
+global photo_timer_duration
+photo_timer_duration = 5
+photo_timer = 0
 wat = Image.open('static/images/watermark.png')
 
 global count
+
+global booth_error
+booth_error = False
 
 state = 0
 
@@ -72,8 +80,45 @@ def watermark(im, mark, position, opacity=1):
     return Image.composite(layer, im, layer)
 
 
+def asyncPictureDone(booth_errorCode):
+    global booth_error
+    if booth_errorCode:
+        print "ASYNC PICTURE DONE WITH ERRORS"
+        booth_error = True
+
+
+def asyncPicture(onExit, filename_arg):
+    """
+    Runs the given args in a subprocess.Popen, and then calls the function
+    onExit when the subprocess completes.
+    onExit is a callable object,and popenArgs is a list/tuple of args that
+    would give to subprocess.Popen.
+    """
+    def runInThread(onExit, filename_arg):
+        photoProcess = subprocess.Popen(['gphoto2', filename_arg, '--capture-image-and-download'], cwd='static/pending_pictures/')
+        # wait for the process to terminate
+        out, err = photoProcess.communicate()
+        errcode = photoProcess.returncode
+        onExit(errcode)
+        return
+    thread = threading.Thread(target=runInThread, args=(onExit, filename_arg))
+    thread.start()
+    # returns immediately after the thread starts
+    return thread
+
+
+def takePicture():
+    # Let's take a picture!!
+    open('state_files/picture_taking', 'a').close()
+    timestamp = time.time()
+    filename_arg = '--filename=%s.jpg' % timestamp
+    asyncPicture(asyncPictureDone, filename_arg)
+
+
 @app.route('/')
 def home():
+    global booth_error
+    booth_error = False
     clean_red_green()
     picture_taking = len(glob.glob('state_files/picture_taking'))
     if picture_taking:
@@ -89,126 +134,150 @@ def ready():
     global last_printing_beginning
     global wat
     global count
+    global photo_timer
+    global photo_timer_duration
+    global booth_error
 
-    with open('count.txt', 'r') as countfile:
-        count = int(countfile.read())
-
-    # Checking pending pictures folder
-    pending_pictures = filter(os.path.isfile, glob.glob('static/pending_pictures/*.jpg'))
-    pending_pictures.sort(key=lambda x: os.path.getmtime(x))
-
-    # Checking printing pictures folder
-    printing_pictures = filter(os.path.isfile, glob.glob('printing_pictures/*.jpg'))
-    printing_pictures.sort(key=lambda x: os.path.getmtime(x))
-
-    # Checking state files
-    picture_taking = len(glob.glob('state_files/picture_taking'))
-
-    response = ''
-
-    if pending_pictures and picture_taking:
-        # Picture taken! we can remove picture_taking file and resize image
-        os.remove('state_files/picture_taking')
-        picture_taking = 0
-        last_picture = pending_pictures[-1]
-
-    if picture_taking:
-        state = 1
-        response = json.dumps(
-            {
-                'state': 1,
-                'html': render_template('taking_picture.html')
-            }
-        )
-    elif pending_pictures:
-        # Pending: showing picture
-        # if red or green, change state
-        if state != 2:
-            # Picture was JUST taken, let's apply watermark
-            with open(pending_pictures[-1], 'rb') as im:
-                print pending_pictures[-1]
-                image = Image.open(im)
-                im_watermarked = watermark(image, wat, (2150, 1300))
-                im_watermarked.save(pending_pictures[-1])
-                del im_watermarked
-                del image
-        state = 2
-        response = json.dumps(
-            {
-                'state': 2,
-                'html': render_template('last_picture.html', last_picture_url=pending_pictures[-1])
-            }
-        )
-        if is_there_green_and_red():
-            # Do nothing
-            pass
-        elif is_there_green():
-            # Print picture
-            if len(pending_pictures) == 1:
-                print "PRINTING LAST PICTURE"
-                last_picture = pending_pictures[-1]
-                last_picture_name = last_picture.split('/')[-1]
-                printing_picture_path = 'printing_pictures/%s' % last_picture_name
-                os.rename(last_picture, printing_picture_path)
-                subprocess.Popen(['lp', '-o', 'media=Postcard(4x6in)', '-o', 'landscape', printing_picture_path])
-                # Print process sent: let's reduce count so we know when there is no more paper
-                set_count(count - 1)
-        elif is_there_red():
-            # Cancel, not printing, archiving
-            print "ARCHIVING PICTURE"
-            last_picture = pending_pictures[-1]
-            last_picture_name = last_picture.split('/')[-1]
-            archived_picture_path = 'archived_pictures/%s' % last_picture_name
-            os.rename(last_picture, archived_picture_path)
-    elif printing_pictures:
-        # After printing time, picture is not printing,
-        # it's printed
-        if state != 3:
-            # last state was not printing!
-            last_printing_beginning = time.time()
-        if state == 3:
-            # last state was already printed
-            # printing has finished after printing_time
-            if time.time() - last_printing_beginning > printing_time:
-                src = printing_pictures[-1]
-                filename = src.split('/')[-1]
-                dest = 'printed_pictures/%s' % filename
-                os.rename(src, dest)
-        state = 3
-        response = json.dumps(
-            {
-                'state': 3,
-                'html': render_template('printing.html')
-            }
-        )
+    if booth_error:
+        state = -1
+        json_response = json.dumps({
+            'state': -1,
+            'html': render_template('error.html')
+        })
     else:
-        if count > 3:
-            state = 0
-            response = json.dumps(
+        with open('count.txt', 'r') as countfile:
+            count = int(countfile.read())
+
+        # Checking pending pictures folder
+        pending_pictures = filter(os.path.isfile, glob.glob('static/pending_pictures/*.jpg'))
+        pending_pictures.sort(key=lambda x: os.path.getmtime(x))
+
+        # Checking printing pictures folder
+        printing_pictures = filter(os.path.isfile, glob.glob('printing_pictures/*.jpg'))
+        printing_pictures.sort(key=lambda x: os.path.getmtime(x))
+
+        # Checking state files
+        picture_taking = len(glob.glob('state_files/picture_taking'))
+
+        json_response = ''
+
+        if pending_pictures and picture_taking:
+            # Picture taken! we can remove picture_taking file and resize image
+            os.remove('state_files/picture_taking')
+            picture_taking = 0
+            last_picture = pending_pictures[-1]
+
+        if picture_taking:
+            state = 1
+            json_response = json.dumps(
                 {
-                    'state': 0,
-                    'html': render_template('ready.html')
+                    'state': 1,
+                    'html': render_template('taking_picture.html')
+                }
+            )
+        elif pending_pictures:
+            # Pending: showing picture
+            # if red or green, change state
+            if state != 2:
+                # Picture was JUST taken, let's apply watermark
+                with open(pending_pictures[-1], 'rb') as im:
+                    print pending_pictures[-1]
+                    image = Image.open(im)
+                    im_watermarked = watermark(image, wat, (2150, 1300))
+                    im_watermarked.save(pending_pictures[-1])
+                    del im_watermarked
+                    del image
+            state = 2
+            json_response = json.dumps(
+                {
+                    'state': 2,
+                    'html': render_template('last_picture.html', last_picture_url=pending_pictures[-1])
                 }
             )
             if is_there_green_and_red():
+                # Do nothing
                 pass
             elif is_there_green():
-                # Let's take a picture!!
-                open('state_files/picture_taking', 'a').close()
-                timestamp = time.time()
-                filename_arg = '--filename=%s.jpg' % timestamp
-                subprocess.Popen(['gphoto2', filename_arg, '--capture-image-and-download'], cwd='static/pending_pictures/')
-        else:
-            state = 4
-            response = json.dumps(
+                # Print picture
+                if len(pending_pictures) == 1:
+                    print "PRINTING LAST PICTURE"
+                    last_picture = pending_pictures[-1]
+                    last_picture_name = last_picture.split('/')[-1]
+                    printing_picture_path = 'printing_pictures/%s' % last_picture_name
+                    os.rename(last_picture, printing_picture_path)
+                    subprocess.Popen(['lp', '-o', 'media=Postcard(4x6in)', '-o', 'landscape', printing_picture_path])
+                    # Print process sent: let's reduce count so we know when there is no more paper
+                    set_count(count - 1)
+            elif is_there_red():
+                # Cancel, not printing, archiving
+                print "ARCHIVING PICTURE"
+                last_picture = pending_pictures[-1]
+                last_picture_name = last_picture.split('/')[-1]
+                archived_picture_path = 'archived_pictures/%s' % last_picture_name
+                os.rename(last_picture, archived_picture_path)
+        elif printing_pictures:
+            # After printing time, picture is not printing,
+            # it's printed
+            if state != 3:
+                # last state was not printing!
+                last_printing_beginning = time.time()
+            if state == 3:
+                # last state was already printed
+                # printing has finished after printing_time
+                if time.time() - last_printing_beginning > printing_time:
+                    src = printing_pictures[-1]
+                    filename = src.split('/')[-1]
+                    dest = 'printed_pictures/%s' % filename
+                    os.rename(src, dest)
+            state = 3
+            json_response = json.dumps(
                 {
-                    'state': 4,
-                    'html': render_template('nopaper.html')
+                    'state': 3,
+                    'html': render_template('printing.html')
                 }
             )
+        else:
+            if count > 3:
+                if state != 5:
+                    state = 0
+                    json_response = json.dumps(
+                        {
+                            'state': 0,
+                            'html': render_template('ready.html')
+                        }
+                    )
+                else:
+                    # We're in timer mode
+                    if time.time() - photo_timer >= photo_timer_duration:
+                        takePicture()
+                    json_response = json.dumps(
+                        {
+                            'state': 5,
+                            'html': render_template('picture_timer.html', timer=max(1, photo_timer_duration - int(time.time() - photo_timer)))
+                        }
+                    )
+                if is_there_green_and_red():
+                    pass
+                elif is_there_green():
+                    state = 5
+                    json_response = json.dumps(
+                        {
+                            'state': 5,
+                            'html': render_template('picture_timer.html', timer=5)
+                        }
+                    )
+                    photo_timer = time.time()
+            else:
+                state = 4
+                json_response = json.dumps(
+                    {
+                        'state': 4,
+                        'html': render_template('nopaper.html')
+                    }
+                )
 
     clean_red_green()
-    return response
+    return json_response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- static/photomat.js
+++ static/photomat.js
@@ -1,10 +1,10 @@
-// state: 0 ready to take picture, 1 taking picture, 2 showing picture, 3 printing picture
-state = -1;
+// state: 0 ready to take picture, 1 taking picture, 2 showing picture, 3 printing picture, 4 nopaper, 5 picture timer, -1 error
+state = -2;
 
 $(document).ready(function() {
     // On vérifie régulièrement si on peut prendre une photo
     // i.e. le dossier pending_pictures est vide
-    loopCheck = setInterval(checkState, 500);
+    loopCheck = setInterval(checkState, 250);
 });
 
 function checkState() {
@@ -12,6 +12,9 @@ function checkState() {
         json = JSON.parse(data);
         newState = parseInt(json.state);
         if (newState != state) {
+            $("#content").html(json.html);
+        }
+        else if (state == 5){
             $("#content").html(json.html);
         }
         state = newState;
